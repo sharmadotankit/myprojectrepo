801dd9d100bae3a315f7a59c3ecbd00e5be300e0
Ankit Sharma <Ankit Sharma@DESKTOP-FT1S12J>
2021-03-23 10:53:43 +0530

    added br1 fun1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -36,8 +36,14 @@ public class Calculator {
 		System.out.println(num1*num1*num1);
 	}
 	
-	
 	public static void mastefun1(int num1){
 		System.out.println(num1*num1*num1);
 	}
+	
+	public static void br1fun1(int num1){
+		System.out.println(num1*num1*num1);
+	}
+	
+	
+	
 }
