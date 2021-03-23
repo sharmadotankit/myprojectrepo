78a44d32953c77dfbf0f3fb59dbf1c4dc6268b34
Ankit Sharma <Ankit Sharma@DESKTOP-FT1S12J>
2021-03-23 09:49:06 +0530

    added div and mod method

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -19,4 +19,12 @@ public class Calculator {
 		System.out.println(num1*num2);
 	}
 	
+	public static void div(int num1,int num2){
+		System.out.println(num1/num2);
+	}
+	
+	public static void mod(int num1,int num2){
+		System.out.println(num1%num2);
+	}
+	
 }
