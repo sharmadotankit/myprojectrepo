99aecdbc7637e927d5abc26d6c30d6ff784030c9
Ankit Sharma <Ankit Sharma@DESKTOP-FT1S12J>
2021-03-23 09:29:55 +0530

    Added prod function in br1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -14,5 +14,9 @@ public class Calculator {
 	public static void diff(int num1,int num2){
 		System.out.println(num1-num2);
 	}
-
+	
+	public static void prod(int num1,int num2){
+		System.out.println(num1*num2);
+	}
+	
 }
